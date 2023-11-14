947f8514d522c4d59d7e60db165bba1bd4286d37
Louis Gerber <louisgerber004@gmail.com>
2023-11-14 23:33:06 +0100

    new filled placeholders

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,18 +6,14 @@ export default function Projects() {
   return (
     <div className="projectsContainer">
       {projectsData.map((project, index) => (
+        project.highlight !== true ? (
         <div key={"project" + index} className={index === 1 ?"projectBloc bloc2" :"projectBloc"}>
           <div className="projectCard">
             <img src={project.images[0]} alt="" />
           </div>
           <div className="projectInfos">
-            <h3 className="projectName">Titre du site - Infos</h3>
-            <p className="description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatum nam, illum corporis iste quidem minus. Rem ducimus
-              officiis tempore error! Velit iste officiis exercitationem magnam
-              architecto animi quam voluptates amet!
-            </p>
+            <h3 className="projectName">{project.titre}</h3>
+            <p className="description">{project.description}</p>
             <div className="usedStacks">
               <h3 className="stacksTitle">Used technology:</h3>
               <p>{project.stacks.join(", ")}</p>
@@ -27,7 +23,7 @@ export default function Projects() {
               <Button icon={<i className="fa-brands fa-github"></i>} text="Check code on Github" />
             </div>
           </div>
-        </div>
+        </div>) : null
       ))}
     </div>
   );
